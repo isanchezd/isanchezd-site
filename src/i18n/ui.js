4678a128418b956ba21fdfed2fbf948887d785fa
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2024-03-24 21:09:00 +0100

    feat: added i18n implementation

---
+++ src/i18n/ui.js
@@ -0,0 +1,23 @@
+export const languages = {
+  es: 'Español',
+  en: 'English',
+};
+
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.about': 'Acerca de',
+    'nav.experience': 'Experiencia',
+    'nav.education': 'Educacion',
+    'nav.certifications': 'Certificaciones',
+    'nav.skills': 'Habilidades',
+  },
+  en: {
+    'nav.about': 'About',
+    'nav.experience': 'Experience',
+    'nav.education': 'Education',
+    'nav.certifications': 'Certifications',
+    'nav.skills': 'Skills',
+  },
+};
