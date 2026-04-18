fe2e55ef39b4c4d4cd374c38018d3017473c9d08
Iván Sánchez <11472842+isanchezd@users.noreply.github.com>
2026-04-18 16:02:36 +0200

    fix: improve typing in i18n utils

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -8,6 +8,6 @@ export function getLangFromUrl(url: { pathname: { split: (arg0: string) => [any,
 
 export function useTranslations(lang: string | number) {
   return function t(key: string | number) {
-    return ui[lang][key] || ui[defaultLang][key];
+    return ui[lang as keyof typeof ui][key as keyof typeof ui[keyof typeof ui]] || ui[defaultLang][key as keyof typeof ui[keyof typeof ui]];
   }
 }
