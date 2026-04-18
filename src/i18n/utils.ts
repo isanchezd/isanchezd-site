2453a780e5cf6051ae9b35cb67d2749bb5f0f674
Iván Sánchez <11472842+isanchezd@users.noreply.github.com>
2026-04-18 16:42:03 +0200

    fix: improve typing and delete unused data atributes

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -1,6 +1,6 @@
 import { ui, defaultLang } from './ui';
 
-export function getLangFromUrl(url: { pathname: { split: (arg0: string) => [any, any]; }; }) {
+export function getLangFromUrl(url: URL) {
   const [, lang] = url.pathname.split('/');
   if (lang in ui) return lang
   return defaultLang;
