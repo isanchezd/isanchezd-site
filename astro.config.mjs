e851471e54a75dcf335fef8df8d218a60e36cf4d
Iván Sánchez <11472842+isanchezd@users.noreply.github.com>
2026-04-18 20:22:37 +0200

    chore: updated to astro v6 (#44)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig, passthroughImageService } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
+  image: { service: passthroughImageService() },
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
